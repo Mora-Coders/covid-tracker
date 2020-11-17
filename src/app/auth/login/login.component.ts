238a5e255eea59d447196f5c675665cba043a0fa
Hasitha Kaushan <hkraclh@gmail.com>
2020-11-17 14:43:54 +0530

    Auth component added

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { NbLoginComponent } from '@nebular/auth';
+
+@Component({
+  selector: 'ngx-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent extends NbLoginComponent {
+
+  ngOnInit(): void {
+  }
+
+}
