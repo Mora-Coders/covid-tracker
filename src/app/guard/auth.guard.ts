d96f88f019766ec6c08e33eefe30ace6233ac07f
Hasitha Kaushan <hkraclh@gmail.com>
2020-12-01 07:52:01 +0530

    logout function and user data to header added

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { AuthService } from 'app/service/auth.service';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +14,21 @@ export class AuthGuard implements CanActivate {
   constructor(private _auth: AuthService, private router: Router){
     this._auth.signedIn$.subscribe(status => {
       this.loggedIn = status;
+      console.log(this.loggedIn);
     });
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if(!this.loggedIn){
-      this.router.navigate(['auth/login']);
-    }
-    return this.loggedIn;
+      return this._auth.isAuthenticated()
+      .pipe(
+        tap(loggedIn => {
+          if (!loggedIn) {
+            this.router.navigate(['/auth/login']);
+          }
+        })
+      );
   }
   
 }
