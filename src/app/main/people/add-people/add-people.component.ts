ae22ba17d33d18770723152b184fb962ad91193f
Hasitha Kaushan <hkraclh@gmail.com>
2020-12-08 09:19:35 +0530

    file upload custom directive added

--- src/app/main/people/add-people/add-people.component.ts
+++ src/app/main/people/add-people/add-people.component.ts
@@ -12,4 +12,17 @@ export class AddPeopleComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  files: any = [];
+
+  uploadFile(event) {
+    for (let index = 0; index < event.length; index++) {
+      console.log("Available");
+      const element = event[index];
+      this.files.push(element.name)
+    }  
+  }
+  deleteAttachment(index) {
+    this.files.splice(index, 1)
+  }
+
 }
--- src/app/main/people/people.module.ts
+++ src/app/main/people/people.module.ts
@@ -5,13 +5,19 @@ import { PeopleRoutingModule } from './people-routing.module';
 import { PeopleComponent } from './people.component';
 import { AddPeopleComponent } from './add-people/add-people.component';
 import { ViewPeopleComponent } from './view-people/view-people.component';
+import { DirectiveModule } from 'app/directive/directive.module';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbIconModule } from '@nebular/theme';
 
 
 @NgModule({
   declarations: [PeopleComponent, AddPeopleComponent, ViewPeopleComponent],
   imports: [
     CommonModule,
-    PeopleRoutingModule
+    PeopleRoutingModule,
+    DirectiveModule,
+    NbIconModule,
+    NbEvaIconsModule
   ]
 })
 export class PeopleModule { }
